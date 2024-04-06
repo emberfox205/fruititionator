a88f81c677a8fa901181735c6786341abf56cf34
Phuc Doan <89180888+emberfox205@users.noreply.github.com>
2024-04-06 14:11:19 +0700

    refactor run.py to use classes from other files

--- run.py
+++ run.py
@@ -1,42 +1,34 @@
 from Fruit_Detectors import detect_fruit
-from API_call import get_api, format_data
-import json, time
-from fruit_shelf import fruit_data
+from API_call import get_api
+import time
 
-def usage():
-    fruit_name = detect_fruit()[0]
-    response = get_api(fruit_name)
-    if response:
-        print(f"{response['description']}: {response['fdcId']}")
-        print(json.dumps(format_data(response), indent=4))
-        return (json.dumps(format_data(response), indent=4), fruit_name)
-    else:
-        return response, fruit_name
-    
-fruit_id = 0
-fruits_detected = []
-while True:
-     nut, name = usage()
-     if nut == {}:
-        print("Unable to get data value, closing...")    
-        break
-     else:
-        fruit_obj = fruit_data(fruit_id, name, nut)
-        fruits_detected.append(fruit_obj)
-        try:
-            prompt = input("input 'e' for rescan, control-d to stop the program: ")
-        except EOFError:
+def get_nutrition():
+    detected_fruit = detect_fruit()
+    return get_api(detected_fruit.name)
+
+def main():
+    scan_count = 0
+    fruits_detected = []
+    while True:
+        fruit_nutrition = get_nutrition()
+        if fruit_nutrition.nutrition == {}:
+            print("Unable to get data value, closing...")    
             break
         else:
+            fruits_detected.append(fruit_nutrition.name)
+            print(fruit_nutrition)
+            prompt = input("input 'e' for rescan, anything else to stop the program: ")
             if prompt == 'e':
                 print("__RESTARTING__")
-                fruit_id += 1
+                scan_count += 1
                 time.sleep(0.5)
             else:
                 print("User timed out")
+                print(f"Scan count {scan_count}")
+                for fruit in fruits_detected:
+                    print(f"Name: {fruit}")
                 break
-
+    
 if __name__ == '__main__':
-    for _ in fruits_detected:
-        print(_)
+    main()
  
