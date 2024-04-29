e1a4e79a95b07b6711a277a434c5ef166cbb8750
Phuc Doan <89180888+emberfox205@users.noreply.github.com>
2024-04-29 16:06:55 +0700

    refactored final result selecting mechanism

--- fruit_detector.py
+++ fruit_detector.py
@@ -1,8 +1,8 @@
 # Responsible for scanning fruit objects.
 
-import time
 import cv2
 import numpy as np
+from collections import Counter
 from keras.models import load_model
 from custom_classes import Detected_Object
 
@@ -18,8 +18,8 @@ def detect_fruit(client, DETECTED_OBJ_FEED_ID, CONFIDENCE_FEED_ID) -> Detected_O
     # Initialize camera
     camera = cv2.VideoCapture(0)
 
-    detected_fruits = []
-    frame_thres = 0
+    detected_results = []
+    frame = 0
     last_class = None
 
     while True:
@@ -40,19 +40,18 @@ def detect_fruit(client, DETECTED_OBJ_FEED_ID, CONFIDENCE_FEED_ID) -> Detected_O
         class_name = class_names[index]
         confidence_score = prediction[0][index]
 
-        print(f"Class: {class_name[2:]}, Confidence Score: {np.round(confidence_score * 100)}, Frame: {frame_thres}")
+        print(f"Class: {class_name[2:]}, Confidence Score: {np.round(confidence_score * 100)}, Frame: {frame}")
 
         # Limiting publishing rate while keeping the camera feed smooth.
-        if frame_thres % 120 == 0 and last_class == class_name[2:]:
+        if frame % 120 == 0 and last_class == class_name[2:]:
             client.publish(CONFIDENCE_FEED_ID, str(np.round(confidence_score * 100)))
-        if frame_thres % 6 == 0 and last_class != class_name[2:]:
+        if frame % 6 == 0 and last_class != class_name[2:]:
             client.publish(CONFIDENCE_FEED_ID, str(np.round(confidence_score * 100)))
             client.publish(DETECTED_OBJ_FEED_ID, class_name[2:])
+            last_class = class_name[2:]
+        frame += 1
 
-        last_class = class_name[2:]
-        frame_thres += 1
-
-        detected_fruits.append({
+        detected_results.append({
             "fruit_name": class_name[2:],
             "confidence_score": float(np.round(confidence_score * 100)),
             "image": cv2.cvtColor(((image[0] + 1) * 127.5), cv2.COLOR_BGR2RGB).astype(np.uint8)  # Save the image for display
@@ -62,18 +61,15 @@ def detect_fruit(client, DETECTED_OBJ_FEED_ID, CONFIDENCE_FEED_ID) -> Detected_O
 
         # 27 is the ASCII for the esc key on your keyboard.
         if keyboard_input == 27:
-            max_score, index = 0, 0
-            last_result = detected_fruits[-2:]
-
-            for i in last_result:
-                if i["confidence_score"] > max_score:
-                    max_score = i["confidence_score"]
-                    index = i
-
-            last_detection = last_result[last_result.index(index)]
-            detected_obj = Detected_Object(last_detection["fruit_name"],
-                                           last_detection["confidence_score"],
-                                           last_detection["image"])
+            latest_results = detected_results[-10:]
+            freq_counter = Counter([result["fruit_name"] for result in latest_results])
+            most_freq_results = [result for result in latest_results if result["fruit_name"] == (freq_counter.most_common(1)[0][0])]
+            best_result = max(most_freq_results, key=lambda result: result["confidence_score"])
+            detected_obj = Detected_Object(best_result["fruit_name"],
+                                           best_result["confidence_score"],
+                                           best_result["image"])
+            client.publish(CONFIDENCE_FEED_ID, best_result["confidence_score"])
+            client.publish(DETECTED_OBJ_FEED_ID, best_result["fruit_name"])
             print(detected_obj)
             camera.release()
             cv2.destroyAllWindows()
